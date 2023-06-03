4fa8e4b1adf9b0ce14c2f278d79ae0b360d7a725
al Mohamady <al.Mohamady47@gmail.com>
2023-06-03 14:10:24 +0300

    014 Form Validations with data annotations

--- BlazorServerAppTest1/Data/Student.cs
+++ BlazorServerAppTest1/Data/Student.cs
@@ -1,15 +1,27 @@
-﻿namespace BlazorServerAppTest1.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerAppTest1.Data
 {
     public class Student
     {
+        [Required(ErrorMessage = "Please Set the student Name")]
         public string StudentName { get; set; }
 
+        [Required(ErrorMessage = "Please Set the student ID")]
         public int StudentId { get; set; }
 
+        [Range(5, 19, ErrorMessage ="age must between 5 , 19")]
         public int StudentAge { get; set; }
-        public string StudentPhone { get; set; }
+
+        [StringLength(10, ErrorMessage = "Incorrect Format for phone number")]
+        public string? StudentPhone { get; set; }
 
         public int StudentGrade { get; set; }
 
+        public DateTime birthdate { get; set; } = DateTime.Now;
+
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "please enter a valid email")]
+        public string? email { get; set; }
     }
 }
